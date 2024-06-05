0f602334acef440f512f9e572aeb316efd051c10
chaithubandi <chaithubandi108@gmail.com>
2024-06-05 20:47:50 +0530

    Add files via upload

---
+++ third-react-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+<div>
+  <h1>gold rates in indian states</h1>
+  <h2>gold rates in hyderabad</h2>
+  <img classname="img" width="200" height="200" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRWQPcVEGb3mQzXcW-6ZpXWMrTovsM6XJYtA&s
+  ">
+  </img>
+ <iframe classname="iframe" width="200" height="200" src="https://en.wikipedia.org/wiki/Gold"></iframe>
+
+ <iframe classname="iframe" width="200" height="200" src="https://www.youtube.com/embed/id848Ww1YLo?si=r-q3g2isz8UmnmxR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+ 
+</div>
+
+  );
+}
+
+export default App;
